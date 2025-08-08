72f180a390f229cc2c5252045c41f72ca58d069e
tisalemasara33-cloud <tisalemasara33@gmail.com>
2025-08-07 19:43:53 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+main.py
+# Programa básico de prueba
+nombre = input("¿Cómo te llamas? ")
+print(f"Hola, {nombre}!")
+
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+print(f"La suma es: {num1 + num2}")
